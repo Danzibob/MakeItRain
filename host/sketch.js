24f8f875fbfe3f46c95ee964f331e924718d91a8
Danny Roberts <danzibob@gmail.com>
2017-04-07 16:53:19 +0100

    Tried.

--- host/sketch.js
+++ host/sketch.js
@@ -1,6 +1,6 @@
-var socket, img, bills, users
+var socket, img, bills, users, cnv, sim
 function setup(){
-	createCanvas(800,400);
+	cnv = createCanvas(800,800);
 	colorMode(HSB)
 	bills = []
 	users = {}
@@ -18,10 +18,15 @@ function setup(){
 		console.log("User dropped")
 		delete users[id]
 	})
+
+	sim = new VerletJS(width, height, canvas)
+	sim.friction = 1
+	sim.highlightColor = "#fff"
 }
 
 function draw(){
 	background(21)
+	sim.frame(20);
 	for(var i = bills.length-1; i >= 0; i--){
 		var kill = bills[i].update()
 		if(kill){bills.splice(i,1)}
@@ -42,8 +47,10 @@ var Bill = function(xPos,speed){
 	this.x = xPos*(width-50) +50
 	console.log(this.x)
 	this.y = height
-	this.vel = speed/4
+	this.vel = speed/10
 	this.size = 100
+	this.segs = 12
+	this.cloth = sim.cloth(new Vec2(this.x,this.y), this.size,this.size*2, this.segs, null, 0.4);
 	this.update = function(){
 		this.y += this.vel
 		if(this.y < -this.size){
@@ -52,6 +59,23 @@ var Bill = function(xPos,speed){
 		return false
 	}
 	this.render = function(){
-		image(img,this.x - this.size/2,this.y,this.size/2,this.size)		
+		//image(img,this.x - this.size/2,this.y,this.size/2,this.size)
+		rect(this.x - this.size/2,this.y,this.size/2,this.size)
+		stroke(0)
+		strokeWeight(1);
+		fill(0,100,100)
+		beginShape(TRIANGLE_STRIP)
+		var x,y;
+		for (y=1;y<this.segs;++y) {
+			for (x=0;x<this.segs;++x) {
+				var i1 = (y-1)*this.segs+x;
+				var i2 = (y)*this.segs+x;
+				var p1 = this.cloth.particles[i1]
+				var p2 = this.cloth.particles[i2]
+				vertex(p1.pos.x,p1.pos.y)
+				vertex(p2.pos.x,p2.pos.y)
+			}
+			endShape()
+		}
 	}
 }
